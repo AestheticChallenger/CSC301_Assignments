d318e04102a85feb4bb395f9c090b405ed4a4665
AestheticChallenger <106012619+AestheticChallenger@users.noreply.github.com>
2024-09-14 19:40:44 +0400

    changed name

--- Goal.java
+++ Goal.java
@@ -1,1 +1,7 @@
+public class Goal {
+  public static void main(String[] args) {
+    
 
+  }
+   
+}
--- budget.java
+++ budget.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class budget {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
         System.out.println("Hello, Wossxsxqrld!");
