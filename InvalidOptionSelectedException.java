623ff878b2992a4ffbfa49e6d0645c4c63f6b648
AestheticChallenger <106012619+AestheticChallenger@users.noreply.github.com>
2024-09-26 23:54:08 +0400

    .

--- InvalidOptionSelectedException.java
+++
@@ -1,6 +0,0 @@
-
-public class InvalidOptionSelectedException extends Exception{
-  public InvalidOptionSelectedException() {
-    super("The option you have selected is invalid. Please try again.\n");
-  }
-}
